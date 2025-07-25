882593fd794e71b7131e29c05b463bf8ab1e851b
dhivagarg-09 <dhivagarg0905@gmail.com>
2025-07-25 14:34:45 +0530

    push

--- MVIT/src/com/MVIT/github/demo.java
+++ MVIT/src/com/MVIT/github/demo.java
@@ -4,7 +4,10 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("NEW PROJECT");
+		System.out.println("updated");
+		System.out.println(101);
+		System.out.println("123");
 	}
 
 }
